6b716c3d0be9fcc7fc118f4d127807058b6b256b
Oleksandr Minakov <minakov13@gmail.com>
2012-11-27 19:52:22 +0200

    Print strings with '\n' and without it

--- PrintStrings/src/PrintStrings.java
+++ PrintStrings/src/PrintStrings.java
@@ -5,8 +5,11 @@ public class PrintStrings {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for (int n = 1; n < 4; n++)
+			System.out.print(" Строка № " + n);
+		System.out.println();
+		for (int n = 1; n < 4; n++)
+			System.out.println("Строка № " + n );
 	}
 
 }
