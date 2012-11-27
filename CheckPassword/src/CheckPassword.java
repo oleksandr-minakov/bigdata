4edfb2860be57ffdfb03734cf3d4db91eb213ffb
Oleksandr Minakov <minakov13@gmail.com>
2012-11-27 20:17:24 +0200

    Check password from command line.

--- CheckPassword/src/CheckPassword.java
+++ CheckPassword/src/CheckPassword.java
@@ -1,3 +1,4 @@
+import java.io.*;
 
 public class CheckPassword {
 
@@ -5,8 +6,21 @@ public class CheckPassword {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String passphrase = "qwerty";
+		InputStreamReader is = new InputStreamReader(System.in);
+		BufferedReader bis = new BufferedReader(is);
+		try {
+			System.out.println("Введите пароль и нажмите <Enter>");
+			String pass = bis.readLine();
+			if (pass.equals(passphrase)) {
+				System.out.println("Пароль верный");
+			} else {
+				System.out.println("Пароль неверный");
+			}
+		} catch(IOException err) {
+			System.err.println("Ошибка " + err);
+		}
+		
 	}
 
 }
