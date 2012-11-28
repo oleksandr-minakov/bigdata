434f22e310479c71cad3ccd5c83a3ce0c2f499ab
Oleksandr Minakov <minakov13@gmail.com>
2012-11-28 15:45:28 +0200

    Applet work, but not in html code

--- DateTaskApplet/src/DateTaskApplet.java
+++ DateTaskApplet/src/DateTaskApplet.java
@@ -1,12 +1,27 @@
+import java.awt.Graphics;
+import java.util.Calendar;
+import java.util.Date;
 
-public class DateTaskApplet {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+public class DateTaskApplet extends javax.swing.JApplet {
+	
+	public String surname = "Minakov";
+	public Date date = new Date();
+	public Calendar dedline = Calendar.getInstance();
+	
+	public void init() {
+		dedline.set(Calendar.YEAR, 2012);
+		dedline.set(Calendar.MONTH, 11);
+		dedline.set(Calendar.DAY_OF_MONTH, 3);
+		setSize(500,300);
 	}
-
+	
+	public void paint(Graphics app) {
+		app.drawString("Разработчик " + surname +
+				" получил задание " + date, 20, 20);
+		app.drawString("Задание должно бьть выполнено к " +
+				dedline.getTime(), 20, 50);
+	}
+	
 }
