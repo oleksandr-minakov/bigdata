21e06ceea8de74335a7ff6941f4d0533fee34bad
Oleksandr Minakov <minakov13@gmail.com>
2012-11-28 14:14:41 +0200

    Work with Date and Calendar

--- DateTask/src/DateTask.java
+++ DateTask/src/DateTask.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+
 
 public class DateTask {
 
@@ -5,7 +8,16 @@ public class DateTask {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String surname = "Minakov";
+		Date date = new Date();
+		System.out.println("Разработчик " + surname +
+				" получил задание " + date);
+		Calendar dedline = Calendar.getInstance();
+		dedline.set(Calendar.YEAR, 2012);
+		dedline.set(Calendar.MONTH, 11);
+		dedline.set(Calendar.DAY_OF_MONTH, 3);
+		System.out.println("Задание должно бьть выполнено к " +
+				dedline.getTime());
 
 	}
 
