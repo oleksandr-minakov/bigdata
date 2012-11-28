93d6ed9f43377ee154d031b3643905761638ee5e
Oleksandr Minakov <minakov13@gmail.com>
2012-11-28 13:16:20 +0200

    Sum arguments from command line and print result.

--- SumArgs/src/SumArgs.java
+++ SumArgs/src/SumArgs.java
@@ -5,8 +5,15 @@ public class SumArgs {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int sum = 0;
+		try {
+			for (int i = 0; i < args.length; i++)
+				sum += Integer.parseInt(args[i]);
+			System.out.println("Сумма равна " + sum);
+		} catch(NumberFormatException err) {
+			System.err.println("Ошибка " + err);
+		}
+		
 	}
 
 }
