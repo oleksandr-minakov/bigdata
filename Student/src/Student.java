ec4ba53562a668ceca9c2bd3ccc29fdb6e249a01
Oleksandr Minakov <minakov13@gmail.com>
2012-12-03 17:39:54 +0200

    Methods addStudent, deleteStudent, findStudent, printList are implemented.

--- Student/src/Student.java
+++ Student/src/Student.java
@@ -1,20 +1,23 @@
 import java.util.*;
+import java.text.*;
 
 public class Student {
 	
 	private int id;
+	private static int next_id = 0;
 	private String name;
 	private String surname;
 	private String patronymic;
-	private Calendar date;
+	private String date;
 	private String address;
-	private long phone;
+	private String phone;
 	private String faculty;
-	private byte course;
+	private int course;
 	private String group;
 	
-	public void setStudentId(int ido) {
-		id = ido;
+	public void setStudentId() {
+		this.id = next_id;
+		next_id++;
 	}	
 	public void setStudentName(String name) {
 		this.name = name;
@@ -25,19 +28,23 @@ public class Student {
 	public void setStudentPatronymic(String patronymic) {
 		this.patronymic = patronymic;
 	}
-	public void setStudentDate(Calendar date) {
-		this.date = date;
+	public void setStudentDate(String date) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM yy", Locale.ROOT);
+		Date d = null;
+		d = sdf.parse(date);
+		this.date = d.toString();
+	
 	}
 	public void setStudentAddress(String address) {
 		this.address = address;
 	}
-	public void setStudentPhone(long phone) {
+	public void setStudentPhone(String phone) {
 		this.phone = phone;		
 	}
 	public void setStudentFaculty(String faculty) {
 		this.faculty = faculty;
 	}
-	public void setStudentCourse(byte course) {
+	public void setStudentCourse(int course) {
 		this.course = course;
 	}
 	public void setStudentGroup(String group) {
@@ -56,78 +63,277 @@ public class Student {
 	public String getStudentPatronymic() {
 		return patronymic;
 	}
-	public Calendar getStudentDate() {
-		return date;
+	public String getStudentDate() {
+		return date.toString();
 	}
 	public String getStudentAddress() {
 		return address;
 	}
-	public long getStudentPhone() {
+	public String getStudentPhone() {
 		return phone;
 	}
 	public String getStudentFaculty() {
 		return faculty;
 	}
-	public byte getStudentCourse() {
+	public int getStudentCourse() {
 		return course;
 	}
 	public String getStudentGroup() {
 		return group;
 	}
+	@Override
 	public String toString() {
-		return "" + surname + "" name + "" patronymic + "" + 
+		return id + " " + surname + " " + name + " " + patronymic + " "  + 
+				date + " " + address + " " + phone + " " + faculty +
+				" " + course + " " + group + " ";
+	}
+
+	
+	public static void addStudent(List<Student> students, Scanner sc) {
+		Student student = new Student();
+		student.setStudentId();
+		System.out.println("Surname");
+		String sn = sc.next();
+		student.setStudentSurname(sn);
+		System.out.println("Name");
+		String n = sc.next();
+		student.setStudentName(n);
+		System.out.println("Patronymic");
+		String p = sc.next();
+		student.setStudentPatronymic(p);
+		System.out.println("Enter date of birth (12 December 1990)");
+		int attempts = 0;
+		sc.nextLine();
+		while(true) {
+			try {
+				String date = sc.nextLine();
+				student.setStudentDate(date);
+				break;
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				System.err.println("Error " + e);
+				attempts++;
+				if(attempts == 5) {
+					System.out.println("Exit ...");
+					sc.close();
+					break;	
+				}
+				System.out.println("Try again");
+				
+			}
+		}
+		System.out.println("Enter the address");
+		String addr = sc.next();
+		student.setStudentAddress(addr);
+		System.out.println("Enter phone");
+		String phone = sc.next();
+		student.setStudentPhone(phone);
+		System.out.println("Enter faculty");
+		student.faculty = sc.next();
+		System.out.println("Enter course (1-6)");
+		student.course = sc.nextByte();
+		System.out.println("Enter group");
+		student.group = sc.next();
+		students.add(student);
+		System.out.println(student.toString());
+
+	}
+	public static void deleteStudent(List<Student> students, Scanner sc) {
+		Student student;
+		if (students.size() > 0) {
+			System.out.println("Students.size = " + students.size());
+			//for(Student s: students) {
+			//	System.out.println("Index of = " + students.indexOf(s));
+			//}
+			System.out.println("Enter ID");
+			int id = sc.nextInt();
+			//if (id >= 0 && id <= (students.size() - 1)) {
+					for(int i = 0; i < (students.size()); i++) {
+						student = students.get(i);
+						if(student.id == id){
+							student.toString();
+							students.remove(student);
+						} else {
+							System.out.println("Wrong ID!!!");
+						}
+					}
+				
+		} else {
+			System.out.println("List is empty!!!");
+		}
+		
+	}
+	public static void findStudent(List<Student> students, Scanner sc) {
+		if (students.size() > 0) {
+			all: while (true) {
+				System.out.println("Find by..." + "\n" + "ID. Press 1." + "\n"
+						+ "surname. Press 2." + "\n" + "name. Press 3." + "\n"
+						+ "patronymic. Press 4." + "\n" + "date. Press 5."
+						+ "\n" + "address. Press 6." + "\n" + "phone. Press 7."
+						+ "\n" + "faculty. Press 8." + "\n"
+						+ "course. Press 9." + "\n" + "group. Press 10." 
+						+ "\n" + "EXIT. Press 11.");
+				int answ = sc.nextInt();
+				Student student;
+				switch (answ) {
+					case 1:
+					System.out.println("Enter ID.");
+					int id = sc.nextInt();
+					if(id > 0 && id < (students.size() - 1)) {
+						student = students.get(id);
+						student.toString();
+					} else {
+						System.out.println("Wrong ID!!!");
+					}
+					break;
+				case 2:
+					System.out.println("Enter surname.");
+					String surname = sc.next();
+					for (int i = 0; i < (students.size() - 1); i++) {
+						student = students.get(i);
+						if (student.surname.equalsIgnoreCase(surname))
+							System.out.println(student.toString());
+					}
+					break;
+				case 3:
+					System.out.println("Enter name.");
+					String name = sc.next();
+					for (int i = 0; i < (students.size() - 1); i++) {
+						student = students.get(i);
+						if (student.name.equalsIgnoreCase(name))
+							System.out.println(student.toString());
+					}
+					break;
+				case 4:
+					System.out.println("Enter patronymic.");
+					String patronymic = sc.next();
+					for (int i = 0; i < (students.size() - 1); i++) {
+						student = students.get(i);
+						if (student.patronymic.equalsIgnoreCase(patronymic))
+							System.out.println(student.toString());
+					}
+					break;
+				case 5:
+					System.out.println("Enter date, (12 December 1990)");
+					for (int i = 0; i < (students.size() - 1); i++) {
+						student = students.get(i);
+						sc.nextLine();
+						String find_date = sc.nextLine();
+						SimpleDateFormat sdf = new SimpleDateFormat(
+								"dd MMMM yy", Locale.ROOT);
+						Date d = null;
+						try {
+							d = sdf.parse(find_date);
+						} catch (ParseException e) {
+							// TODO Auto-generated catch block
+							//e.printStackTrace();
+							System.err.println("Error " + e);
+							System.out.println("Error");
+							break;
+						}
+						String string_date = d.toString();
+						if (student.date.toString().equalsIgnoreCase(
+								string_date))
+							System.out.println(student.toString());
+					}
+					break;
+				case 6:
+					System.out.println("Enter address.");
+					String address = sc.next();
+					for (int i = 0; i < (students.size() - 1); i++) {
+						student = students.get(i);
+						if (student.address.equalsIgnoreCase(address))
+							System.out.println(student.toString());
+					}
+					break;
+				case 7:
+					System.out.println("Enter phone.");
+					String phone = sc.next();
+					for (int i = 0; i < (students.size() - 1); i++) {
+						student = students.get(i);
+						if (student.phone.equalsIgnoreCase(phone))
+							System.out.println(student.toString());
+					}
+					break;
+				case 8:
+					System.out.println("Enter faculty.");
+					String faculty = sc.next();
+					for (int i = 0; i < (students.size() - 1); i++) {
+						student = students.get(i);
+						if (student.faculty.equalsIgnoreCase(faculty))
+							System.out.println(student.toString());
+					}
+					break;
+				case 9:
+					System.out.println("Enter course.");
+					int course = sc.nextInt();
+					for (int i = 0; i < (students.size() - 1); i++) {
+						student = students.get(i);
+						if (student.course == course)
+							System.out.println(student.toString());
+					}
+					break;
+				case 10:
+					System.out.println("Enter group.");
+					String group = sc.next();
+					for (int i = 0; i < (students.size() - 1); i++) {
+						student = students.get(i);
+						if (student.group.equalsIgnoreCase(group))
+							System.out.println(student.toString());
+					}
+					break;
+				case 11: 
+					break all;
+				default:
+					break all;
+				}
+			}
+		} else { 
+			System.out.println("List is empty!!!");
+		}
+	}
+	public static void printList(List<Student> students) {
+		for(Student s: students) {
+			System.out.println(s.toString());
+		}
 	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student student;
-		String answ1 = "ДА";
-		String answ2 = "НЕТ";
-		List<Student> students = new ArrayList<Student>();
-		System.out.println("Хотите ввести данные о студенте? Наберите в командной строке" +
-				"ДА или НЕТ.");
 		Scanner sc = new Scanner(System.in);
-		String answ = sc.next();
-		while(true) {
-			if(answ.equals(answ1)) {
-				student = new Student();
-				System.out.println("Введите имя");
-				String n = sc.next();
-				student.setStudentName(n);
-				System.out.println("Введите фамилию");
-				String sn = sc.next();
-				student.setStudentSurname(sn);
-				System.out.println("Введите отчество");
-				String p = sc.next();
-				student.setStudentPatronymic(p);
-				System.out.println("Введите дату рождения, нажимая <ENTER> после каждого числа" +
-						"год, месяц, число");
-				Calendar date = Calendar.getInstance();
-				date.set(sc.nextInt(), sc.nextInt(), sc.nextInt());
-				student.setStudentDate(date);
-				System.out.println("Введите адрес");
-				String addr = sc.next();
-				student.setStudentAddress(addr);
-				System.out.println("Введите телефон (только цифры)");
-				long phone = sc.nextLong();
-				student.setStudentPhone(phone);
-				System.out.println("Введите факультет");
-				student.faculty = sc.next();
-				System.out.println("Введите курс (1-6)");
-				student.course = sc.nextByte();
-				System.out.println("Введите группу");
-				student.group = sc.next();
-				students.add(student);
-				for(Student s: students) {
-					System.out.println()
-				}
-				
+		List<Student> students = new ArrayList<Student>();
+		all:
+		while (true) {
+			System.out.println("Select the action:" + "\n"
+					+ "Add student. Press 1." + "\n"
+					+ "Delete student. Press 2." + "\n"
+					+ "Find student. Press 3." + "\n" + "Show list. Press 4."
+					+ "\n" + "Exit. Press 5.");
+			int answ = sc.nextInt();
+			switch (answ) {
+			case 1:
+				addStudent(students, sc);
+				continue all ;
+			case 2:
+				deleteStudent(students, sc);
+				continue all;
+			case 3:
+				findStudent(students, sc);
+				continue all;
+			case 4:
+				printList(students);
+				continue all;
+			case 5:
+				System.out.println("Exit...");
+				break all;
+			default:
+				System.out.println("Exit...");
+				break all;
 			}
-			
 		}
-		
-	}
+	}					
 
 }
+
