f03cf5d11ae4cebd85693870539d74f35c2ddbf3
Oleksandr Minakov <minakov13@gmail.com>
2012-12-18 17:03:27 +0200

    Improve UPS and change Find.

--- Student/src/com/mirantis/aminakov/cli/Cli.java
+++ Student/src/com/mirantis/aminakov/cli/Cli.java
@@ -88,123 +88,30 @@ public class Cli {
 			case 3:
 				HashMap<String, String> findMap = new HashMap<>();
 				System.out.println("Enter name of field and value (name=Vasya or phone=123456789)");
-				String findPar = sc.next();
+				sc.nextLine();
+				String findPar = sc.nextLine();
+//				StringTokenizer st = new StringTokenizer(findPar, "=");
 				int i;
 				i = findPar.indexOf('=');
 				String part1 = findPar.substring(0, i);
-				String part2 = findPar.substring(i+1);
-//				System.out.println("Part 1 = " + part1);
-//				System.out.println("Part 2 = " + part2);
+				String part2 = findPar.substring(i+1, findPar.length());
+				/*while (st.hasMoreTokens()) {
+					part1 = st.nextToken();
+					part2 = st.nextToken();
+					System.out.println(part1);
+					System.out.println(part2);
+					findMap.put(part1, part2);
+				}*/
+				System.out.println("Key = " + part1);
+				System.out.println("Value = " + part2);
 				findMap.put(part1, part2);
 				Student std = Student.buildExampleStudent();
-//				System.out.println(std);
 				UniversalSetter u = new UniversalSetter(std);
 				u.setFields(findMap);
 				System.out.println(std);
-					/*while (true) {
-					System.out.println("Find by..." + "\n" + "ID. Press 1." + "\n"
-							+ "surname. Press 2." + "\n" + "name. Press 3." + "\n"
-							+ "patronymic. Press 4." + "\n" + "date. Press 5."
-							+ "\n" + "address. Press 6." + "\n" + "phone. Press 7."
-							+ "\n" + "faculty. Press 8." + "\n"
-							+ "course. Press 9." + "\n" + "group. Press 10." 
-							+ "\n" + "EXIT. Press 11.");
-					int answer = sc.nextInt();
-					Student stud = Student.buildExampleStudent();
-					switch (answer) {
-						case 1:
-						System.out.println("Enter ID.");
-						int idFind = sc.nextInt();
-						stud.setId(idFind);
-						for(Student s : storage.findByExample(stud)) {
-							System.out.println(s.toString());
-						}
-						continue;
-					case 2:
-						System.out.println("Enter surname.");
-						String sur = sc.next();
-						stud.setSurname(sur);
-						for(Student s : storage.findByExample(stud)) {
-							System.out.println(s.toString());
-						}
-						continue;
-					case 3:
-						System.out.println("Enter name.");
-						String nam = sc.next();
-						stud.setName(nam);
-						for(Student s : storage.findByExample(stud)) {
-							System.out.println(s.toString());
-						}
-						continue;
-					case 4:
-						System.out.println("Enter patronymic.");
-						String pat = sc.next();
-						stud.setPatronymic(pat);
-						for(Student s : storage.findByExample(stud)) {
-							System.out.println(s.toString());
-						}
-						continue;
-					case 5:
-						System.out.println("Enter date, (12 December 1990)");
-						sc.nextLine();
-						String find_date = sc.nextLine();
-						try {
-							stud.setDate(find_date);
-						} catch (ParseException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						for(Student s : storage.findByExample(stud)) {
-							System.out.println(s.toString());
-						}
-						continue;
-					case 6:
-						System.out.println("Enter address.");
-						String adr = sc.next();
-						stud.setAddress(adr);
-						for(Student s : storage.findByExample(stud)) {
-							System.out.println(s.toString());
-						}					
-						continue;
-					case 7:
-						System.out.println("Enter phone.");
-						String phn = sc.next();
-						stud.setPhone(phn);
-						for(Student s : storage.findByExample(stud)) {
-							System.out.println(s.toString());
-						}
-						continue;
-					case 8:
-						System.out.println("Enter faculty.");
-						String fac = sc.next();
-						stud.setFaculty(fac);
-						for(Student s : storage.findByExample(stud)) {
-							System.out.println(s.toString());
-						}
-						continue;
-					case 9:
-						System.out.println("Enter course.");
-						int crs = sc.nextInt();
-						stud.setCourse(crs);
-						for(Student s : storage.findByExample(stud)) {
-							System.out.println(s.toString());
-						}
-						continue;
-					case 10:
-						System.out.println("Enter group.");
-						String grp = sc.next();
-						stud.setGroup(grp);
-						for(Student s : storage.findByExample(stud)) {
-							System.out.println(s.toString());
-						}
-						continue;
-					case 11: 
-						break;
-					default:
-						break;
-					}
-					break;
-				}*/
+				for(Student s : storage.findByExample(std)) {
+					System.out.println(s.toString());
+				}
 				continue;
 			case 4:
 				storage.printList(os);	
--- Student/src/com/mirantis/aminakov/ups/UniversalSetter.java
+++ Student/src/com/mirantis/aminakov/ups/UniversalSetter.java
@@ -27,25 +27,56 @@ public class UniversalSetter {
 		Field[] fields = c.getDeclaredFields();
 		for (Field field : fields) {
 			InputIgnore i = field.getAnnotation(InputIgnore.class);
-			if (i.value() == false) {
+			if (i.value() == true) {
+//				System.out.println(field.getName() + " IGNOGE FOR INPUT");
+			} else {
+//				System.out.println(field.getName() + " INPUT THIS FIELD");
 				if (!(map.get(field.getName()) == null)) {
 					field.setAccessible(true);
 					String type = field.getType().getSimpleName();
-					type.toLowerCase();
-					switch(type) {
-					case "int":
-						int value = Integer.parseInt(map.get(field.getName()));
-					}
 					try {
-						field.set(this.o, map.get(field.getName()));
-					} catch (IllegalArgumentException | IllegalAccessException e) {
-						// TODO Auto-generated catch block
+						switch(type.toLowerCase()) {
+						case "int":
+							int intVal = Integer.parseInt(map.get(field.getName()));
+							field.setInt(this.o, intVal);
+							break;
+						case "string":
+							String strVal = map.get(field.getName());
+							field.set(this.o, strVal);
+							break;
+						case "boolean":
+							boolean boolVal = Boolean.parseBoolean(map.get(field.getName()));
+							field.setBoolean(this.o, boolVal);
+							break;
+						case "byte":
+							byte byteVal = Byte.parseByte(map.get(field.getName()));
+							field.setByte(this.o, byteVal);
+							break;
+						case "char":
+							char charVal = map.get(field.getName()).charAt(0);
+							field.setChar(this.o, charVal);
+							break;
+						case "short":
+							short shortVal = Short.parseShort(map.get(field.getName()));
+							field.setShort(this.o, shortVal);
+							break;
+						case "long":
+							long longVal = Long.parseLong(map.get(field.getName()));
+							field.setLong(this.o, longVal);
+							break;
+						case "double":
+							double doubleVal = Double.parseDouble(map.get(field.getName()));
+							field.setDouble(this.o, doubleVal);
+							break;
+						case "float":
+							float floatVal = Float.parseFloat(map.get(field.getName()));
+							field.setFloat(this.o, floatVal);
+							break;
+						}
+					} catch ( IllegalArgumentException| IllegalAccessException e) {
 						e.printStackTrace();
 					}
 				}
-//				System.out.println(field.getName() + " IGNOGE FOR INPUT");
-			} else {
-//				System.out.println(field.getName() + " INPUT THIS FIELD");	
 			}
 		}
 		return o;
