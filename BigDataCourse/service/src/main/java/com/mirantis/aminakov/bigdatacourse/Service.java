43147fa4d482d4c1f6e83ca902b57147166682d6
Oleksandr Minakov <minakov13@gmail.com>
2013-01-17 17:28:47 +0200

    Change method.

--- BigDataCourse/service/src/main/java/com/mirantis/aminakov/bigdatacourse/Service.java
+++ BigDataCourse/service/src/main/java/com/mirantis/aminakov/bigdatacourse/Service.java
@@ -1,6 +1,7 @@
 package com.mirantis.aminakov.bigdatacourse;
 
 import java.util.List;
+import java.util.TreeSet;
 
 public interface Service {
 	public int addBook(Book book);
@@ -8,5 +9,5 @@ public interface Service {
 	public List<Book> findByTitle(int pageNum, int pageSize, String title);
 	public List<Book> findByText(int pageNum, int pageSize, String text);
 	public List<Book> findByGenre(int pageNum, int pageSize, String genre);
-	public List<String> findAuthorByGenre(int pageNum, int pageSize, String genre);
+	public TreeSet<String> findAuthorByGenre(int pageNum, int pageSize, String genre);
 }
