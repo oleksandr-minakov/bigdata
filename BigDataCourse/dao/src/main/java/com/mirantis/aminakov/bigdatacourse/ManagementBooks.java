7b35fff3c88fe6c616f3f4614b33f6eda616c3b0
Oleksandr Minakov <minakov13@gmail.com>
2013-01-23 18:37:18 +0200

    Change arr from ru to en.

--- BigDataCourse/dao/src/main/java/com/mirantis/aminakov/bigdatacourse/ManagementBooks.java
+++ BigDataCourse/dao/src/main/java/com/mirantis/aminakov/bigdatacourse/ManagementBooks.java
@@ -24,8 +24,8 @@ public class ManagementBooks {
 		genres = new ArrayList<String>();
 		texts = new ArrayList<InputStream>();
 		
-		char[] arr = { 'а', 'б', 'в', 'г', 'д', ' ', 'е', 'ё', 'ж', 'з', 'и', ' ', 'й', 'к', 'л', 'м', 'н',
-                ' ', 'о', 'п', 'р', 'с', 'т', ' ', 'у', 'ф', 'х', 'ц', 'ч', 'ш', ' ', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я' };
+		char[] arr = { 'a', 'b', 'c', 'd', 'e', ' ', 'f', 'g', 'h', 'i', 'j', ' ', 'k', 'l', 'm', 'n', 'o',
+                ' ', 'p', 'q', 'r', 's', 't', ' ', 'u', 'v', 'w', 'x', 'y', 'z' };
         int length = 0; 
         Random rand = new Random();
         for (int i = 0; i < 50; i++) {
