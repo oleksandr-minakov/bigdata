95f31b8dfd95cfe2d22f9ba2fc74f21ed629854c
aminakov <aminakov@aminakov-pc.local>
2013-02-18 15:46:55 +0200

    Change mysql port to 3306.

--- BigDataCourse/dao/src/main/java/com/mirantis/aminakov/bigdatacourse/dao/ManagementTables.java
+++ BigDataCourse/dao/src/main/java/com/mirantis/aminakov/bigdatacourse/dao/ManagementTables.java
@@ -10,7 +10,7 @@ import org.apache.log4j.Logger;
 public class ManagementTables {
 	public static final Logger LOG = Logger.getLogger(ManagementTables.class);
 	String driverName = "com.mysql.jdbc.Driver";
-	String url = "jdbc:mysql://localhost:1234/bigdata?user=aminakov&password=bigdata";
+	String url = "jdbc:mysql://localhost:3306/bigdata?user=aminakov&password=bigdata";
 	String jdbcutf8 = "&useUnicode=true&characterEncoding=UTF-8";
 	protected Connection con = null;
 	Statement st = null;
