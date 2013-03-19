79b29c458eeba320867a854e1b9c2334bfa07e25
crazymac <lildee1991@gmail.com>
2013-03-19 16:11:53 +0200

    remove unneeded imports

--- BigDataCourse/dao/src/main/java/com/mirantis/aminakov/bigdatacourse/dao/cassandra/BookConverter.java
+++ BigDataCourse/dao/src/main/java/com/mirantis/aminakov/bigdatacourse/dao/cassandra/BookConverter.java
@@ -3,7 +3,6 @@ package com.mirantis.aminakov.bigdatacourse.dao.cassandra;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
