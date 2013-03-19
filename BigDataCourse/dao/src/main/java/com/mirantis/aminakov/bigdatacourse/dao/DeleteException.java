590f9a2aa22a6a7c7da5ed4ee793f4d4730ec559
Aleksandr Minakov <aminakov@mirantis.com>
2013-03-19 10:23:30 +0200

    Refactoring. Delete unused things.

--- BigDataCourse/dao/src/main/java/com/mirantis/aminakov/bigdatacourse/dao/DeleteException.java
+++ BigDataCourse/dao/src/main/java/com/mirantis/aminakov/bigdatacourse/dao/DeleteException.java
@@ -4,7 +4,6 @@ public class DeleteException extends DaoException {
 
 	public DeleteException(Exception e) {
 		super(e);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public DeleteException (String msg) {
