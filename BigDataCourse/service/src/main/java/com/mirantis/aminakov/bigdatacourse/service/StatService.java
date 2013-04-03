26bcf1ff05d516d9fd51f866a097d0b3367a4933
crazymac <lildee1991@gmail.com>
2013-04-03 15:00:03 +0300

    Configuring service to be ran with Job class bean, Mapper class bean, Reducer class bean.

--- BigDataCourse/service/src/main/java/com/mirantis/aminakov/bigdatacourse/service/StatService.java
+++ BigDataCourse/service/src/main/java/com/mirantis/aminakov/bigdatacourse/service/StatService.java
@@ -8,12 +8,15 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.Reducer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import com.mirantis.aminakov.bigdatacourse.dao.DaoException;
 import com.mirantis.aminakov.bigdatacourse.dao.hadoop.configuration.HadoopConnector;
 import com.mirantis.aminakov.bigdatacourse.dao.hadoop.configuration.Pair;
 import com.mirantis.aminakov.bigdatacourse.mapreduce.GetParsedStatistics;
 import com.mirantis.aminakov.bigdatacourse.mapreduce.JobRunner;
+import com.mirantis.aminakov.bigdatacourse.mapreduce.MapReduceThread;
 
 public class StatService {
 	
@@ -25,6 +28,10 @@ public class StatService {
 	private Object reducer;
 	@Autowired
 	private HadoopConnector configuration;
+	
+	private MapReduceThread mapRedThread;
+	private ThreadPoolTaskExecutor pool;
+	private TaskExecutor exec;
 	
 	public Object getJob() {
 		return job;
@@ -51,11 +58,30 @@ public class StatService {
 		this.configuration = configuration;
 	}
 	
+	
+	private void setUpService(){
+		
+		mapRedThread = new MapReduceThread();
+			mapRedThread.setConfiguration(this.configuration);
+			mapRedThread.setJobClass(this.job);
+			mapRedThread.setMapperClass(this.mapper);
+			mapRedThread.setReducerClass(this.reducer);
+		
+		pool = new ThreadPoolTaskExecutor();
+			pool.setCorePoolSize(1);
+			pool.setMaxPoolSize(1);
+			pool.createThread(mapRedThread);
+			pool.setQueueCapacity(1);
+			pool.setWaitForTasksToCompleteOnShutdown(true);
+			pool.initialize();
+		
+		exec = pool;
+	}
 	@SuppressWarnings("rawtypes")
 	public List<Pair<String, Double>> viewStatistics() throws IOException, DaoException{
 		
 		List<Pair<String, Double>> pairs = new ArrayList<Pair<String, Double>>();
-		if(this.configuration.getFS().exists(new Path("/Statistics")) && this.configuration.getFS().listStatus(new Path("/Statistics")).length != 1){
+		if(this.configuration.getFS().exists(new Path("/Statistics")) && this.configuration.getFS().listStatus(new Path("/Statistics")).length > 1){
 			
 			GetParsedStatistics  parser = new GetParsedStatistics(this.configuration);
 			JobRunner jobRunner = new JobRunner(this.configuration, job.getClass(), ((Mapper)mapper).getClass(), ((Reducer)reducer).getClass());
@@ -69,7 +95,6 @@ public class StatService {
 		
 	}
 	
-	@SuppressWarnings("rawtypes")
 	public List<Pair<String, Double>> recalculatetatistics() throws IOException, DaoException{
 		
 		List<Pair<String, Double>> pairs = new ArrayList<Pair<String, Double>>();
@@ -77,10 +102,8 @@ public class StatService {
 		if(this.configuration.getFS().exists(new Path("/Statistics")))
 			this.configuration.getFS().delete(new Path("/Statistics"), true);
 		
-		JobRunner jobRunner = new JobRunner(this.configuration, job.getClass(), ((Mapper)mapper).getClass(), ((Reducer)reducer).getClass());
-		GetParsedStatistics  getP = new GetParsedStatistics(this.configuration);
-		Path path = jobRunner.getPathToEvaluatedStatistics();
-		pairs = getP.getParsedStatistics(path);
+		setUpService();
+		exec.execute(this.mapRedThread);
 		
 		return pairs;
 	}
