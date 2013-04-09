519172d3e3f8ca96f8b5ba4d64945e9d93b5eb96
Aleksandr Minakov <aminakov@mirantis.com>
2013-04-09 17:34:05 +0300

    Add getBookByAuthorTest.

---
+++ BigDataCourse/dao/src/test/java/com/mirantis/aminakov/bigdatacourse/dao/solr/GetBookByAuthorTest.java
@@ -0,0 +1,31 @@
+package com.mirantis.aminakov.bigdatacourse.dao.solr;
+
+import com.mirantis.aminakov.bigdatacourse.dao.Book;
+import com.mirantis.aminakov.bigdatacourse.dao.DaoException;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class GetBookByAuthorTest {
+
+    @Test
+    public void getBookByAuthorTest() throws FileNotFoundException, DaoException {
+        List<Book> books = new ArrayList<Book>();
+        Parameters parameters = new Parameters("http://localhost:8080/solr-web");
+        DaoSolr daoSolr = new DaoSolr(parameters);
+        Book book = new Book();
+        book.newBook("title", "author", "genre", new FileInputStream("testbook"));
+        int id = daoSolr.addBook(book);
+        try {
+            books = daoSolr.getBookByAuthor(1, 1, "author");
+            assertTrue(books.get(0).getAuthor().equals(book.getAuthor()));
+        } finally {
+            daoSolr.delBook(id);
+        }
+    }
+}
