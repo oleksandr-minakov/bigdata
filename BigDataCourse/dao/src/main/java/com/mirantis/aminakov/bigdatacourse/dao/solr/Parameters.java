b462638467e3b543fa6e04facce38ae6bbd41d13
Aleksandr Minakov <aminakov@mirantis.com>
2013-04-11 15:06:02 +0300

    Add DaoNAS to parameters.

--- BigDataCourse/dao/src/main/java/com/mirantis/aminakov/bigdatacourse/dao/solr/Parameters.java
+++ BigDataCourse/dao/src/main/java/com/mirantis/aminakov/bigdatacourse/dao/solr/Parameters.java
@@ -1,13 +1,17 @@
 package com.mirantis.aminakov.bigdatacourse.dao.solr;
 
+import com.mirantis.aminakov.bigdatacourse.dao.NAS.DaoNAS;
+
 public class Parameters {
 
     final String URL;
 
     public int bookId;
+    DaoNAS daoNAS;
 
-    public Parameters(String url) {
-        URL = url;
+    public Parameters(String url, DaoNAS daoNAS) {
+        this.URL = url;
+        this.daoNAS = daoNAS;
         this.bookId = 0;
     }
 }
