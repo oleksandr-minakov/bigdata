53ecb9e5a01bdfac0d50b296efe66b89de074c2b
crazymac <lildee1991@gmail.com>
2013-05-08 16:50:48 +0300

    dfg

--- BigDataCourse/dao/src/test/java/com/mirantis/aminakov/bigdatacourse/dao/cassandratests/CassandraIP.java
+++ BigDataCourse/dao/src/test/java/com/mirantis/aminakov/bigdatacourse/dao/cassandratests/CassandraIP.java
@@ -2,6 +2,6 @@ package com.mirantis.aminakov.bigdatacourse.dao.cassandratests;
 
 public class CassandraIP {
 
-	public static String IP1 = "0.0.0.0";
-	public static String IP2 = "0.0.0.0"; 
+	public static String IP2 = "0.0.0.0";
+	public static String IP1 = "0.0.0.0"; 
 }
