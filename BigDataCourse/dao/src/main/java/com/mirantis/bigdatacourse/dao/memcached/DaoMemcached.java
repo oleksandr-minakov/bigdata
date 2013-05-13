5a5cac996dc2167a9471ab04dbfe54cfe82ae111
Aleksandr Minakov <aminakov@mirantis.com>
2013-05-13 16:07:40 +0300

    Fix memcached for a new model.

--- BigDataCourse/dao/src/main/java/com/mirantis/bigdatacourse/dao/memcached/DaoMemcached.java
+++ BigDataCourse/dao/src/main/java/com/mirantis/bigdatacourse/dao/memcached/DaoMemcached.java
@@ -3,11 +3,8 @@ package com.mirantis.bigdatacourse.dao.memcached;
 import com.mirantis.bigdatacourse.dao.Book;
 import com.mirantis.bigdatacourse.dao.Dao;
 import com.mirantis.bigdatacourse.dao.DaoException;
+import com.mirantis.bigdatacourse.dao.PaginationModel;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeSet;
 
 public class DaoMemcached implements Dao {
     @Autowired
@@ -15,8 +12,6 @@ public class DaoMemcached implements Dao {
 
     @Autowired
     private Dao daoJdbc;
-
-    private int numberOfRecords = -1;
 
     public void setDaoJdbc(Dao dao) {
         this.daoJdbc = dao;
@@ -44,170 +39,98 @@ public class DaoMemcached implements Dao {
 
     @SuppressWarnings("unchecked")
 	@Override
-    public List<Book> getAllBooks(int pageNum, int pageSize) throws DaoException {
-        List<Book> books = new ArrayList<Book>();
+    public PaginationModel getAllBooks(int pageNum, int pageSize) throws DaoException {
+        PaginationModel model = new PaginationModel();
         String keystring = pageNum + "allBooks" + pageSize;
-        String numberOfRecordsKey = keystring + "records";
-        books = (List<Book>) client.get(Integer.toString(keystring.hashCode()));
-        Object o = client.get(Integer.toString(numberOfRecordsKey.hashCode()));
-        if (o != null) {
-            numberOfRecords = (int) o;
-        }
-        if (books != null ) {
-            return books;
+        model = (PaginationModel) client.get(Integer.toString(keystring.hashCode()));
+        if (model != null ) {                                                         //TODO maybe if(!model.getBooks().isEmpty())
+            return model;
         } else {
-            books = daoJdbc.getAllBooks(pageNum, pageSize);
-            int records = daoJdbc.getNumberOfRecords("", "");
-            if (!books.isEmpty()) {
-                client.set(Integer.toString(keystring.hashCode()), 6, books);
-                books = (List<Book>) client.get(Integer.toString(keystring.hashCode()));
-                client.set(Integer.toString(numberOfRecordsKey.hashCode()), 6, records);
-                numberOfRecords = (int) client.get(Integer.toString(numberOfRecordsKey.hashCode()));
+            model = daoJdbc.getAllBooks(pageNum, pageSize);
+            if (!model.getBooks().isEmpty()) {
+                client.set(Integer.toString(keystring.hashCode()), 6, model);
+                model = (PaginationModel) client.get(Integer.toString(keystring.hashCode()));
             }
         }
-        return books;
+        return model;
     }
 
     @SuppressWarnings("unchecked")
 	@Override
-    public List<Book> getBookByTitle(int pageNum, int pageSize, String title) throws DaoException {
-        List<Book> books = new ArrayList<Book>();
+    public PaginationModel getBookByTitle(int pageNum, int pageSize, String title) throws DaoException {
+        PaginationModel model = new PaginationModel();
         String keystring = pageNum + title + "getBookByTitle" + pageSize;
-        String numberOfRecordsKey = keystring + "records";
-        books = (List<Book>) client.get(Integer.toString(keystring.hashCode()));
-        Object o = client.get(Integer.toString(numberOfRecordsKey.hashCode()));
-        if (o != null) {
-            numberOfRecords = (int) o;
-        }
-        if (books != null) {
-            return books;
+        model = (PaginationModel) client.get(Integer.toString(keystring.hashCode()));
+        if (model != null) {
+            return model;
         } else {
-            books = daoJdbc.getBookByTitle(pageNum, pageSize, title);
-            int records = daoJdbc.getNumberOfRecords("", "");
-            if (!books.isEmpty()) {
-                client.set(Integer.toString(keystring.hashCode()), 6, books);
-                books = (List<Book>) client.get(Integer.toString(keystring.hashCode()));
-                client.set(Integer.toString(numberOfRecordsKey.hashCode()), 6, records);
-                numberOfRecords = (int) client.get(Integer.toString(numberOfRecordsKey.hashCode()));
+            model = daoJdbc.getBookByTitle(pageNum, pageSize, title);
+            if (!model.getBooks().isEmpty()) {
+                client.set(Integer.toString(keystring.hashCode()), 6, model);
+                model = (PaginationModel) client.get(Integer.toString(keystring.hashCode()));
             }
         }
-        return books;
+        return model;
     }
 
     @SuppressWarnings("unchecked")
 	@Override
-    public List<Book> getBookByText(int pageNum, int pageSize, String text) throws DaoException {
-        List<Book> books = new ArrayList<Book>();
+    public PaginationModel getBookByText(int pageNum, int pageSize, String text) throws DaoException {
+        PaginationModel model = new PaginationModel();
         String keystring = pageNum + "getBookByText" + text + pageSize;
-        String numberOfRecordsKey = keystring + "records";
-        books = (List<Book>) client.get(Integer.toString(keystring.hashCode()));
-        Object o = client.get(Integer.toString(numberOfRecordsKey.hashCode()));
-        if (o != null) {
-            numberOfRecords = (int) o;
-        }
-        if (books != null) {
-            return books;
+        model = (PaginationModel) client.get(Integer.toString(keystring.hashCode()));
+        if (model != null) {
+            return model;
         } else {
-            books = daoJdbc.getBookByText(pageNum, pageSize, text);
-            int records = daoJdbc.getNumberOfRecords("", "");
-            if (!books.isEmpty()) {
-                client.set(Integer.toString(keystring.hashCode()), 6, books);
-                books = (List<Book>) client.get(Integer.toString(keystring.hashCode()));
-                client.set(Integer.toString(numberOfRecordsKey.hashCode()), 6, records);
-                numberOfRecords = (int) client.get(Integer.toString(numberOfRecordsKey.hashCode()));
+            model = daoJdbc.getBookByText(pageNum, pageSize, text);
+            if (!model.getBooks().isEmpty()) {
+                client.set(Integer.toString(keystring.hashCode()), 6, model);
+                model = (PaginationModel) client.get(Integer.toString(keystring.hashCode()));
             }
         }
-        return books;
+        return model;
     }
 
     @SuppressWarnings("unchecked")
 	@Override
-    public List<Book> getBookByAuthor(int pageNum, int pageSize, String author) throws DaoException {
-        List<Book> books = new ArrayList<Book>();
+    public PaginationModel getBookByAuthor(int pageNum, int pageSize, String author) throws DaoException {
+        PaginationModel model = new PaginationModel();
         String keystring = pageNum + "getBookByAuthor" + author + pageSize;
-        String numberOfRecordsKey = keystring + "records";
-        books = (List<Book>) client.get(Integer.toString(keystring.hashCode()));
-        Object o = client.get(Integer.toString(numberOfRecordsKey.hashCode()));
-        if (o != null) {
-            numberOfRecords = (int) o;
-        }
-        if (books != null) {
-            return books;
+        model = (PaginationModel) client.get(Integer.toString(keystring.hashCode()));
+        if (model != null) {
+            return model;
         } else {
-            books = daoJdbc.getBookByAuthor(pageNum, pageSize, author);
-            int records = daoJdbc.getNumberOfRecords("", "");
-            if (!books.isEmpty()) {
-                client.set(Integer.toString(keystring.hashCode()), 6, books);
-                books = (List<Book>) client.get(Integer.toString(keystring.hashCode()));
-                client.set(Integer.toString(numberOfRecordsKey.hashCode()), 6, records);
-                numberOfRecords = (int) client.get(Integer.toString(numberOfRecordsKey.hashCode()));
+            model = daoJdbc.getBookByAuthor(pageNum, pageSize, author);
+            if (!model.getBooks().isEmpty()) {
+                client.set(Integer.toString(keystring.hashCode()), 6, model);
+                model = (PaginationModel) client.get(Integer.toString(keystring.hashCode()));
             }
         }
-        return books;
+        return model;
     }
 
     @SuppressWarnings("unchecked")
 	@Override
-    public List<Book> getBookByGenre(int pageNum, int pageSize, String genre) throws DaoException {
-        List<Book> books = new ArrayList<Book>();
+    public PaginationModel getBookByGenre(int pageNum, int pageSize, String genre) throws DaoException {
+        PaginationModel model = new PaginationModel();
         String keystring = pageNum + "getBookByGenre" + genre + pageSize;
-        String numberOfRecordsKey = keystring + "records";
-        books = (List<Book>) client.get(Integer.toString(keystring.hashCode()));
-        Object o = client.get(Integer.toString(numberOfRecordsKey.hashCode()));
-        if (o != null) {
-            numberOfRecords = (int) o;
-        }
-        if (books != null) {
-            return books;
+        model = (PaginationModel) client.get(Integer.toString(keystring.hashCode()));
+        if (model != null) {
+            return model;
         } else {
-            books = daoJdbc.getBookByGenre(pageNum, pageSize, genre);
-            int records = daoJdbc.getNumberOfRecords("", "");
-            if (!books.isEmpty()) {
-                client.set(Integer.toString(keystring.hashCode()), 6, books);
-                books = (List<Book>) client.get(Integer.toString(keystring.hashCode()));
-                client.set(Integer.toString(numberOfRecordsKey.hashCode()), 6, records);
-                numberOfRecords = (int) client.get(Integer.toString(numberOfRecordsKey.hashCode()));
+            model = daoJdbc.getBookByGenre(pageNum, pageSize, genre);
+            if (!model.getBooks().isEmpty()) {
+                client.set(Integer.toString(keystring.hashCode()), 6, model);
+                model = (PaginationModel) client.get(Integer.toString(keystring.hashCode()));
             }
         }
-        return books;
-    }
-
-    @Override
-    public TreeSet<String> getAuthorByGenre(int pageNum, int pageSize, String genre) throws DaoException {
-        TreeSet<String> authors = new TreeSet<String>();
-        String keystring = pageNum + "getAuthorByGenre" + genre + pageSize;
-        String numberOfRecordsKey = keystring + "records";
-        authors = (TreeSet<String>) client.get(Integer.toString(keystring.hashCode()));
-        Object o = client.get(Integer.toString(numberOfRecordsKey.hashCode()));
-        if (o != null) {
-            numberOfRecords = (int) o;
-        }
-        if (authors != null) {
-            return client.pagination(pageNum, pageSize, authors);
-        } else {
-            authors = daoJdbc.getAuthorByGenre(pageNum, pageSize, genre);
-            int records = daoJdbc.getNumberOfRecords("", "");
-            if (!authors.isEmpty()) {
-                client.set(Integer.toString(keystring.hashCode()), 6, authors);
-                authors = (TreeSet<String>) client.get(Integer.toString(keystring.hashCode()));
-                authors = client.pagination(pageNum, pageSize, authors);
-                client.set(Integer.toString(numberOfRecordsKey.hashCode()), 6, records);
-                numberOfRecords = (int) client.get(Integer.toString(numberOfRecordsKey.hashCode()));
-            }
-        }
-        authors = daoJdbc.getAuthorByGenre(pageNum, pageSize, genre);
-        return authors;
+        return model;
     }
 
     @Override
     public void closeConnection() throws DaoException {
         daoJdbc.closeConnection();
         client.close();
-    }
-
-    @Override
-    public int getNumberOfRecords(String whereToSeek, String whatToSeekFor) {
-        return this.numberOfRecords;
     }
 
     @Override
