60a39c48213d62c6a174e9c34c6b728fe0666b4d
Aleksandr Minakov <aminakov@mirantis.com>
2013-05-23 18:35:27 +0300

    Refactoring. Change Solr for right use injections and parametrs. Delete parameters replaced by @Value.

--- BigDataCourse/dao/src/main/java/com/mirantis/bigdatacourse/dao/solr/BookUtils.java
+++ BigDataCourse/dao/src/main/java/com/mirantis/bigdatacourse/dao/solr/BookUtils.java
@@ -2,14 +2,14 @@ package com.mirantis.bigdatacourse.dao.solr;
 
 import com.mirantis.bigdatacourse.dao.Book;
 import com.mirantis.bigdatacourse.dao.DaoException;
-import com.mirantis.bigdatacourse.dao.NAS.NASMapping;
+import com.mirantis.bigdatacourse.dao.FSMapping;
 import org.apache.solr.common.SolrDocument;
 
 import java.io.IOException;
 
 public class BookUtils {
 
-    public static Book map(SolrDocument document, NASMapping daoNAS) throws IOException, DaoException {
+    public static Book map(SolrDocument document, FSMapping daoNAS) throws IOException, DaoException {
         Book book = new Book();
         book.setId(document.get("id").toString());
         book.setTitle(document.get("title").toString());
