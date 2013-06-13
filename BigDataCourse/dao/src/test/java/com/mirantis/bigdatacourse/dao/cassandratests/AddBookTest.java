a17aad156bbd95e3a9b2e2a472a61cc40f510bd9
crazymac <lildee1991@gmail.com>
2013-06-13 17:46:17 +0300

    upd

--- BigDataCourse/dao/src/test/java/com/mirantis/bigdatacourse/dao/cassandratests/AddBookTest.java
+++ BigDataCourse/dao/src/test/java/com/mirantis/bigdatacourse/dao/cassandratests/AddBookTest.java
@@ -17,21 +17,26 @@ public class AddBookTest {
 	@Test
 	public void addBookTest() throws IOException, DaoException{
 		
-		List<String> hosts = new ArrayList<>();
-		hosts.add(CassandraIP.IP1);
-		hosts.add(CassandraIP.IP2);
-		hosts.add(CassandraIP.IP3);
+		Constants cts = null;
+		try{
+			List<String> hosts = new ArrayList<>();
+			hosts.add(CassandraIP.IP1);
+			hosts.add(CassandraIP.IP2);
+			hosts.add(CassandraIP.IP3);
+			
+			cts = new Constants("Cassandra Cluster", "KS", "Test", hosts.get(0));
+			
+			DaoCassandra dao = new DaoCassandra(cts);
+			for(int i = 0; i < 100; ++i) {
+				Book book = new Book();
+				book.newBook("CassandraTest", "Test" + i, "Tester" + i, new FileInputStream("testbook"));
+				dao.addBook(book);
+			}
+			Assert.assertNotNull(cts);
+			Assert.assertNotNull(dao);
 		
-		Constants cts = new Constants("Cassandra Cluster", "KS", "Test", hosts.get(0));
-		
-		DaoCassandra dao = new DaoCassandra(cts);
-		for(int i = 0; i < 100; ++i) {
-			Book book = new Book();
-			book.newBook("CassandraTest", "Test" + i, "Tester" + i, new FileInputStream("testbook"));
-			dao.addBook(book);
+		} finally {
+			cts.getCurrentClstr().dropKeyspace("KS");
 		}
-		Assert.assertNotNull(cts);
-		Assert.assertNotNull(dao);
-		cts.getCurrentClstr().dropKeyspace("KS");
 	}
 }
